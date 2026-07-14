eef2e9d54b351ffe62f178e009214a805a97239d
Sivaram <sivadevimohan@gmail.com>
2026-07-14 14:45:35 +0530

    Update hangman.py

--- hangman.py
+++ hangman.py
@@ -21,26 +21,26 @@ while not game_over:
   print(f"****************************{lives}/6 LIVES LEFT****************************")
   guess = input("Guess a letter: ").lower()
   display = ""
-    if guess in correct_letters:
-        print(f"You've already guessed {guess}.")
-    for letter in chosen_word:
-        if letter == guess:
-            display += letter
-            correct_letters.append(guess)
-        elif letter in correct_letters:
-            display += letter
-        else:
-            display += "_"
-    guessed_letters.append(guess)
-    print("Word to guess: " + display)
-    if guess not in chosen_word:
-      print(f"You guessed {guess}, that's not in the word. You lose a life.")
-      lives -= 1
-      if lives == 0:
-          game_over = True
-          print(f"***********************IT WAS {chosen_word}!YOU LOSE**********************")
-    if "_" not in display:
+if guess in correct_letters:
+    print(f"You've already guessed {guess}.")
+for letter in chosen_word:
+    if letter == guess:
+        display += letter
+        correct_letters.append(guess)
+    elif letter in correct_letters:
+        display += letter
+    else:
+        display += "_"
+guessed_letters.append(guess)
+print("Word to guess: " + display)
+if guess not in chosen_word:
+    print(f"You guessed {guess}, that's not in the word. You lose a life.")
+    lives -= 1
+    if lives == 0:
         game_over = True
-        print("****************************YOU WIN****************************")
-    stages = hangman_art.stages
-    print(stages[lives])
+        print(f"***********************IT WAS {chosen_word}!YOU LOSE**********************")
+if "_" not in display:
+    game_over = True
+    print("****************************YOU WIN****************************")
+stages = hangman_art.stages
+print(stages[lives])
